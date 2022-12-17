173fb8bc6509815912130629c6cdb75b2e8644dd
ABHI <114732679+AbhiDevOfficial@users.noreply.github.com>
2022-12-17 22:21:19 +0530

    Update yt.js

--- data/yt.js
+++ data/yt.js
@@ -1,5 +1,8 @@
 const ytdl = require('ytdl-core')
-const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg')
+const yts = require('yt-search')
+const ytdl = require('ytdl-core')
+const fs = require('fs')
 
 function youtubeVideoDownload(url) {
  if (!url) throw new Error("URL_ARG : Missing 'url' argument.")
@@ -17,3 +20,30 @@ function youtubeVideoDownload(url) {
  return readVideo;
 }
 
+function youtubeAudioDownload(url) {
+ if (!url) throw new Error("URL_ARG : Missing 'url' argument.")
+ if (!url.startsWith('http://') && !url.startsWith('https://') && !url.includes('youtu')) throw new Error("INV_URL : Invalid url.")
+ if (!url.includes('watch')) url = url.replace('watch?v=', '')
+ let reader;
+ try {
+   reader = ytdl(url, { filter: 'audioonly' });
+ } catch {
+   reader = false;
+ }
+ if (!reader) throw new Error("INV_URL : Invalid url or the entered url has a problem.")
+ let audio = ffmpeg(reader).format('mp3').audioBitrate('128').save('yta-'+url+'.mp3').on('end', () => fs.readFileSync('yta-'+url+'.mp3'))
+ return audio;
+}
+
+function youtubeSearch(query) {
+ return new Promise((resolve, reject) => {
+  if (!query) throw new Error("QUERY_ARG : Missing 'query' argument.")
+  try {
+    var ytsResult = await yts(query);
+  } catch {
+    throw new Error("NOT_FOUND : Couldn't find result for this query.")
+  }
+  let result = ytsResult.all
+  resolve({ status: true, creator: 'AbhiDevOfficial', npm: '@abhidevofficial/simple-apis', message: 'success', result: result })
+ })
+}
