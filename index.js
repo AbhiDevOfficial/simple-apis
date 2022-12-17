5fc422b40064e0ae9b7e3f36f0d8aadae029d436
ABHI <114732679+AbhiDevOfficial@users.noreply.github.com>
2022-12-17 22:24:21 +0530

    Exported

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { generateWaChatUrl } = require('./data/wame')
-const { igPostDownload, igStoryDownload } = require('./data/instagram')
+const { yts: youtubeSearch, ytv: youtubeVideoDownload, yta: youtubeAudioDownload } = require('./data/yt')
+// const { igPostDownload, igStoryDownload } = require('./data/instagram')
 
 module.exports = {
-  igPostDownload: igPostDownload,
-  igStoryDownload: igStoryDownload,
+  youtubeSearch: yts,
+  youtubeVideoDownload: ytv,
+  youtubeAudioDownload: yta,
   generateWaChatUrl: generateWaChatUrl
 };
