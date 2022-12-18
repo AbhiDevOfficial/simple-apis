b533bc46fc47552f5a4e44fc55ff78359cc2f172
ABHI <114732679+AbhiDevOfficial@users.noreply.github.com>
2022-12-18 15:10:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const { generateWaChatUrl } = require('./data/wame')
-const { youtubeSearch, youtubeVideoDownload, youtubeAudioDownload } = require('./data/yt')
-// const { igPostDownload, igStoryDownload } = require('./data/instagram')
 
 module.exports = {
-  youtubeSearch: youtubeSearch,
-  youtubeVideoDownload: youtubeVideoDownload,
-  youtubeAudioDownload: youtubeAudioDownload,
   generateWaChatUrl: generateWaChatUrl
 };
