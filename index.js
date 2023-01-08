ea6842eb36749070f6d51650f7f84a215702a266
ABHI <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-01-08 20:20:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { generateDirectLink } = require('./data/smlink')
 const { imageToPDF } = require('./data/pdf')
+const { youtubeSearch, youtubeVideoDownload } = require('./data/yt')
 
 module.exports = {
   imageToPDF: imageToPDF,
+  youtubeSearch: youtubeSearch,
+  youtubeVideoDownload: youtubeVideoDownload,
   generateDirectLink: generateDirectLink
 };
