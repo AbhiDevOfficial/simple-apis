3863030848f4672a173aa2e2c2b981f43a4ffe43
ABHI <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-01-08 21:22:02 +0530

    Update yt.js

--- data/yt.js
+++ data/yt.js
@@ -1,5 +1,5 @@
 const yts = require('yt-search')
-const ytld = require('ytld-core')
+const ytdl = require('ytdl-core')
 const fs = require('fs')
 
 async function youtubeSearch(q) {
@@ -12,18 +12,46 @@ async function youtubeSearch(q) {
 async function youtubeVideoDownload(q) {
  var VID = '';
  try {
-   if (q.includes('watch')) {
-     var tsts = q.replace('watch?v=', '')
+   if (q.startsWith('https://') && q.includes('youtu')) {
+     if (q.includes('/shorts/')) VID = q.replace('shorts/', '').split('/')[3]
+     var tsts = q.replace(/[watch?v=]/g, '')
      var alal = tsts.split('/')[3]
      VID = alal
-   } else {     
-     VID = q.split('/')[3]
-   }
-  } catch {
+   } else {
+     VID = await youtubeSearch(q);
+     VID = VID[0].url
+     var tsts = VID.replace(/[watch?v=]/g, '')
+     var alal = tsts.split('/')[3]
+     VID = alal
+ } catch {
     return false;
-  }
+ }
   var yt = ytdl(VID, {filter: format => format.container === 'mp4' && ['720p', '480p', '360p', '240p', '144p'].map(() => true)})
   yt.pipe(fs.createWriteStream('./' + VID + '.mp4'))
   var result = yt.on('end', async () => fs.readFileSync('./' + VID + '.mp4'))
+  return result;
+}
+
+
+async function youtubeAudioDownload(q) {
+ var VID = '';
+ try {
+   if (q.startsWith('https://') && q.includes('youtu')) {
+     if (q.includes('/shorts/')) VID = q.replace('shorts/', '').split('/')[3]
+     var tsts = q.replace(/[watch?v=]/g, '')
+     var alal = tsts.split('/')[3]
+     VID = alal
+   } else {
+     VID = await youtubeSearch(q);
+     VID = VID[0].url
+     var tsts = VID.replace(/[watch?v=]/g, '')
+     var alal = tsts.split('/')[3]
+     VID = alal
+ } catch {
+    return false;
+ }
+  var yt = ytdl(VID, {filter: format => format.container === 'mp3' })
+  yt.pipe(fs.createWriteStream('./' + VID + '.mp3'))
+  var result = yt.on('end', async () => fs.readFileSync('./' + VID + '.mp3'))
   return result;
 }
